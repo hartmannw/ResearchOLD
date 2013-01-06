64b5f251cda7e50cd345f37c2281b733dbd52bb6
hartmanw <hartmannw@gmail.com>
2013-01-06 13:24:32 +0100

    Added support for the Matrix class in the MatrixFunctions set of utilities. Closes #2

--- Utilities/MatrixFunctions.cc
+++ Utilities/MatrixFunctions.cc
@@ -1,1 +1,8 @@
+// William Hartmann (hartmannw@gmail.com)
+// This is free and unencumbered software released into the public domain.
+// See the UNLICENSE file for more information.
+
 #include "MatrixFunctions.h"
+
+// All functions have template arguments, so the implementation is in the
+// corresponding .h file.
--- Utilities/MatrixFunctions.h
+++ Utilities/MatrixFunctions.h
@@ -1,17 +1,29 @@
+// William Hartmann (hartmannw@gmail.com)
+// This is free and unencumbered software released into the public domain.
+// See the UNLICENSE file for more information.
+
 #ifndef UTILITIES_MATRIXFUNCTIONS_
 #define UTILITIES_MATRIXFUNCTIONS_
 
 #include<vector>
-#include<cstdlib>
 #include<cmath>
 #include<stdio.h>
 #include<string>
 #include<iostream>
 #include<fstream>
+#include "Matrix.h"
+
+// Utility functions and functions commonly used for matrices. Most functions
+// overloaded to accept either a matrix object or a vector of vectors. Note that
+// many of the functions that operate on matrix objects are slower than
+// necessary because they simply extract a vector of vectors from the matrix and
+// call the corresponding version of the code.
 
 namespace utilities
 {
 
+// Prints out the matrix in a row/col format. The format string corresponds to
+// the format in the printf function.
 template <typename T> 
 void PrintMatrix(const std::vector<std::vector<T> > &matrix, std::string format)
 {
@@ -26,6 +38,14 @@ void PrintMatrix(const std::vector<std::vector<T> > &matrix, std::string format)
   }
 }
 
+template <typename T> 
+void PrintMatrix(const Matrix<T> &matrix, std::string format)
+{
+  std::vector<std::vector<T> > m = matrix.GetVectorOfVectors();
+  PrintMatrix(m, format);
+}
+
+// Allows only a subset of the full matrix to be easily printed.
 template <typename T> 
 void PrintSubMatrix(const std::vector<std::vector<T> > &matrix, 
     unsigned int startr, unsigned startc, unsigned int rows, 
@@ -42,6 +62,16 @@ void PrintSubMatrix(const std::vector<std::vector<T> > &matrix,
   }
 }
 
+template <typename T> 
+void PrintSubMatrix(const Matrix<T> &matrix, 
+    unsigned int startr, unsigned startc, unsigned int rows, 
+    unsigned int cols, std::string format)
+{
+  std::vector<std::vector<T> > m;
+  m = matrix.GetVectorOfVectors();
+  PrintSubMatrix(m, startr, startc, rows, cols, format);
+}
+
 template <typename T>
 void MatrixAbs(std::vector<std::vector<T> > &matrix)
 {
@@ -52,6 +82,16 @@ void MatrixAbs(std::vector<std::vector<T> > &matrix)
 }
 
 template <typename T>
+void MatrixAbs(Matrix<T> &matrix)
+{
+  for(unsigned int r = 0; r < matrix.NumRows(); ++r)
+    for(unsigned int c = 0; c < matrix.NumCols(); ++c)
+      matrix(r,c) = std::abs(matrix(r,c));
+}
+
+// Frobenius norm is the square root of the sums of the squares of each element.
+// sqrt( sum( x^2) ).
+template <typename T>
 double FrobeniusNorm (const std::vector<std::vector<T> > &matrix)
 {
   double ret = 0;
@@ -60,6 +100,13 @@ double FrobeniusNorm (const std::vector<std::vector<T> > &matrix)
       ret += static_cast<double>(matrix[i][j] * matrix[i][j]);
 
   return std::sqrt(ret);
+}
+
+template <typename T>
+double FrobeniusNorm (const Matrix<T> &matrix)
+{
+  std::vector<std::vector<T> > m = matrix.GetVectorOfVectors();
+  return FrobeniusNorm(m);
 }
 
 template <typename T>
@@ -74,6 +121,7 @@ void InitializeMatrix(std::vector<std::vector<T> > &matrix, T value,
   }
 }
 
+// Every element in the matrix is 0, except for the diagonal, which are 1.
 template <typename T>
 void CreateIdentityMatrix(std::vector<std::vector<T> > &matrix, 
     unsigned int rows, unsigned int cols)
@@ -86,6 +134,16 @@ void CreateIdentityMatrix(std::vector<std::vector<T> > &matrix,
     matrix[i][i] = 1;
 }
 
+template<typename T>
+void CreateIdentityMatrix(Matrix<T> &matrix, unsigned int rows, 
+    unsigned int cols)
+{
+  matrix.Initialize(rows, cols, 0);
+  matrix.SetDiagonal(1);
+}
+
+// Loops through the entire matrix to find the maximal element. Also gives the 
+// location of the element.
 template <typename T>
 T MaxElementInMatrix(std::vector<std::vector<T> > &matrix, unsigned int &row, 
     unsigned int &col)
@@ -105,6 +163,16 @@ T MaxElementInMatrix(std::vector<std::vector<T> > &matrix, unsigned int &row,
 }
 
 template <typename T>
+T MaxElementInMatrix(const Matrix<T> &matrix, unsigned int &row, 
+    unsigned int &col)
+{
+  std::vector<std::vector<T> > m = matrix.GetVectorOfVectors();
+  return MaxElementInMatrix(m, row, col);
+}
+
+// Loops through the entire matrix to find the minimal element. Also gives the 
+// location of the element.
+template <typename T>
 T MinElementInMatrix(std::vector<std::vector<T> > &matrix, unsigned int &row, 
     unsigned int &col)
 {
@@ -123,6 +191,15 @@ T MinElementInMatrix(std::vector<std::vector<T> > &matrix, unsigned int &row,
 }
 
 template <typename T>
+T MinElementInMatrix(const Matrix<T> &matrix, unsigned int &row, 
+    unsigned int &col)
+{
+  std::vector<std::vector<T> > m = matrix.GetVectorOfVectors();
+  return MinElementInMatrix(m, row, col);
+}
+
+// Standard matrix transposition.
+template <typename T>
 std::vector<std::vector<T> > TransposeMatrix(
     std::vector<std::vector<T> > &matrix)
 {
@@ -138,6 +215,21 @@ std::vector<std::vector<T> > TransposeMatrix(
 }
 
 template <typename T>
+Matrix<T> TransposeMatrix(const Matrix<T> &matrix)
+{
+  std::vector<std::vector<T> > ret, m;
+  Matrix<T> matrix_ret;
+  m = matrix.GetVectorOfVectors();
+  ret = TransposeMatrix(m);
+  matrix_ret.Initialize(ret);
+  return matrix_ret;
+}
+
+// Uses a simple, but inefficient matrix product implementation. If this is
+// something you need to do often with large matrices, you should be using a
+// more powerful matrix library. Function does not check that the sizes of the
+// matrices are valid for the product.
+template <typename T>
 std::vector<std::vector<T> > MatrixProduct(
     std::vector<std::vector<T> > &A, std::vector<std::vector<T> > &B)
 {
@@ -147,6 +239,18 @@ std::vector<std::vector<T> > MatrixProduct(
     for(unsigned int j = 0; j < B[i].size(); ++j)
       for(unsigned int m = 0; m < B.size(); ++m)
         ret[i][j] += ( A[i][m] * B[m][j] );
+  return ret;
+}
+
+template <typename T>
+Matrix<T> MatrixProduct(const Matrix<T> &A, const Matrix<T> &B)
+{
+  std::vector<std::vector<T> > a = A.GetVectorOfVectors();
+  std::vector<std::vector<T> > b = B.GetVectorOfVectors();
+  std::vector<std::vector<T> > prod;
+  Matrix<T> ret;
+  prod = MatrixProduct(a, b);
+  ret.Initialize(prod);
   return ret;
 }
 
@@ -186,6 +290,8 @@ std::vector<std::vector<T> > UpperTriangularMatrix(
   return ret;     
 }
 
+// Saves the matrix to a file in the row/col format. Each value is separate by a
+// space and each row is given a newline.
 template <typename T>
 void SaveMatrixToTextFile(std::vector<std::vector<T> > &matrix, 
     std::string filename)
@@ -202,5 +308,13 @@ void SaveMatrixToTextFile(std::vector<std::vector<T> > &matrix,
 
   fout.close();
 }
+
+template <typename T>
+void SaveMatrixToTextFile(const Matrix<T> &matrix, std::string filename)
+{
+  std::vector<std::vector<T> > m = matrix.GetVectorOfVectors();
+  SaveMatrixToTextFile(m, filename);
 }
+
+} // end namespace utilities
 #endif
