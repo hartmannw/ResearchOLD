ef47109421d4ff59ae8f4b1a54ff23416c48f479
hartmanw <hartmannw@gmail.com>
2013-01-08 12:09:07 +0100

    Tested the changes to SpeechFeatures.

--- FileUtilities/test.cc
+++ FileUtilities/test.cc
@@ -6,17 +6,17 @@
 int main()
 {
   fileutilities::SpeechFeatures sf;
- // std::string filename = "/people/hartmann/research/SegmentalModel/tidigit_exp/data/htkplp/train/man/ae/12a.mlp";
-  std::string filename = "/people/hartmann/research/src/AcousticUnitDiscovery/utt1.cep.mvn";
-  std::string fileout = "example.htk2";
-  sf.ReadCepFile(filename);
+  std::string filename = "test_data/example.htk2";
+  std::string fileout = "test_data/example.htk2";
+  sf.ReadHtkFile(filename);
 
   std::vector<double> mean = sf.CalculateRecordMean(0);
   for(int i = 0; i < mean.size(); ++i)
     std::cout<<mean[i]<<std::endl;
   sf.WriteHtkFile(fileout);
 
-  fileutilities::WriteBinaryPGM(sf.record(0), std::string("example.pgm"));
+  fileutilities::WriteBinaryPGM(sf.record(0).GetVectorOfVectors(), 
+      std::string("test_data/example.pgm"));
 
   return 0;
 }
